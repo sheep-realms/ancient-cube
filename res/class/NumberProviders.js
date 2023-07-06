81b05ce53f582e39f89009e3fd3b4eb9d65bd9dd
Sheep-realms <284178977@qq.com>
2023-07-06 17:59:17 +0800

    Update

--- res/class/NumberProviders.js
+++ res/class/NumberProviders.js
@@ -1,19 +1,39 @@
 class NumberProviders {
-    constructor(type = 'constant', data = {}) {
-        this.type = 'constant';
+    constructor(data = {}) {
         this.data = {};
 
-        this.create(type, data);
+        this.create(data);
     }
 
-    create(type, data) {
-        this.type = type;
-        if (typeof data == 'number') data = { value: data };
-        this.data = data;
+    create(data) {
+        if (typeof data == 'number') {
+            this.data = {
+                type: 'constant',
+                value: data
+            };
+        } else if (typeof data == 'object') {
+            switch (data.type) {
+                case 'constant':
+                case 'uniform':
+                // case 'gaussian':
+                case 'lens':
+                    this.data = data;
+                    break;
+            
+                default:
+                    break;
+            }
+        } else {
+            this.data = {
+                type: 'NaN'
+            };
+        }
+
+        return this.data;
     }
 
     getValue(...initValue) {
-        switch (this.type) {
+        switch (this.data.type) {
             // 常数
             case 'constant':
                 return this.data?.value;
@@ -21,6 +41,14 @@ class NumberProviders {
             // 随机平均分布
             case 'uniform':
                 return Math.round(Math.random() * (this.data?.max - this.data?.min) + this.data?.min);
+
+            // 随机正态分布
+            // case 'gaussian':
+            //     let u = 0, v = 0;
+            //     while (u == 0) u = Math.random();
+            //     while (v == 0) v = Math.random();
+            //     let z = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
+            //     return Math.round(z * this.data.std + this.data.value);
 
             // 透镜
             case 'lens':
